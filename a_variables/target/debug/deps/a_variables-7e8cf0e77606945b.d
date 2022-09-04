25bd4412073f4c2e329e8d800434cba3914f2dea
Muhammad Ilham Hidayat <m.ilham.hidayat.95@gmail.com>
2022-09-04 17:34:12 +0700

    feat(a_variables): make variables project

---
+++ a_variables/target/debug/deps/a_variables-7e8cf0e77606945b.d
@@ -0,0 +1,5 @@
+/Users/ilham/project/rust/rust-crash-course/a_variables/target/debug/deps/a_variables-7e8cf0e77606945b.rmeta: src/main.rs
+
+/Users/ilham/project/rust/rust-crash-course/a_variables/target/debug/deps/a_variables-7e8cf0e77606945b.d: src/main.rs
+
+src/main.rs:
---
+++ a_variables/target/debug/deps/a_variables-f263437c30144d41.d
@@ -0,0 +1,5 @@
+/Users/ilham/project/rust/rust-crash-course/a_variables/target/debug/deps/a_variables-f263437c30144d41.rmeta: src/main.rs
+
+/Users/ilham/project/rust/rust-crash-course/a_variables/target/debug/deps/a_variables-f263437c30144d41.d: src/main.rs
+
+src/main.rs:
