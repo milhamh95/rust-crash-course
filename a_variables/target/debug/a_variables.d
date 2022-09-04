c8567e6390b299d02292edb9c73b856a234dd109
Muhammad Ilham Hidayat <m.ilham.hidayat.95@gmail.com>
2022-09-04 17:39:46 +0700

    feat(a_variables): initialize and print variables

---
+++ a_variables/target/debug/a_variables.d
@@ -0,0 +1,1 @@
+/Users/ilham/project/rust/rust-crash-course/a_variables/target/debug/a_variables: /Users/ilham/project/rust/rust-crash-course/a_variables/src/main.rs
---
+++ a_variables/target/debug/deps/a_variables-6b9438d23594aad6.d
@@ -0,0 +1,5 @@
+/Users/ilham/project/rust/rust-crash-course/a_variables/target/debug/deps/a_variables-6b9438d23594aad6: src/main.rs
+
+/Users/ilham/project/rust/rust-crash-course/a_variables/target/debug/deps/a_variables-6b9438d23594aad6.d: src/main.rs
+
+src/main.rs:
